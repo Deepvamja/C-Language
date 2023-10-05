00ca48d78b1c8a3e34c592242bbea5b8dab810c0
Deepvamja <143236575+Deepvamja@users.noreply.github.com>
2023-10-05 11:17:45 +0530

    Add files via upload

---
+++ ascii character with their value.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+	
+	int i;
+	for (i=65;i<=90;i++){
+		printf("%c  %d\n",i,i);
+		
+	}
+	for(i=97;i<=122;i++){
+		printf("%c  %d\n",i,i);
+	}
+}
---
+++ fibonacci series.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+	
+	void main ()
+{
+
+  int first = 0, second = 1, third, i, n;
+
+  printf ("Enter the length of the fibonacci series \n");
+
+  scanf ("%d", &n);
+
+  printf ("The Fibonacci series is :\n");
+
+  printf ("%d%d", first, second);
+
+  for (i = 0; i <= n; i++)
+
+    {
+
+      third = first + second;
+
+      printf ("%d ", third);
+
+      first = second;
+
+      second = third;
+  }
+
+}
